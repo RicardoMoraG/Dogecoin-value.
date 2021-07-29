9f4485a46c291ca88060e7ef13df8e314e9c3cb5
Ricardo Mora <60626665+RicardoMoraG@users.noreply.github.com>
2021-07-28 20:52:08 -0600

    Add files via upload

---
+++ Doge_Value.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+__author__ = "Ricardo Mora"
+
+# used libraries
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import csv
+import pandas as pd
+
+# get datetime
+date = str(datetime.date.today())
+
+# target website
+url = "https://coinmarketcap.com/currencies/dogecoin/markets/"
+
+# make GET request
+req = requests.get(url)
+
+# we pass the url content to a bs4 object
+html_cont = BeautifulSoup(req.text, "html.parser") 
+
+# we get all the divs were the needed entries are
+entry = html_cont.find_all('div', {'class': 'sc-57oli2-0 dEqHl cmc-body-wrapper'})
+entry_rank = html_cont.find_all('div', {'class': 'sc-16r8icm-0 bILTHz'})
+
+# we recieve the data needed from the divs
+for i, entries in enumerate(entry):
+    doge_price = entries.find('div', {'class': 'priceValue___11gHJ'}).text
+for i, entries in enumerate((entry_rank)):
+    doge_rank = entries.find('div', {'class': 'namePill___3p_Ii namePillPrimary___2-GWA'}).text
+print(date," - ", doge_price, " - ", doge_rank) # test print
+
+# pandas dataframe to send the data to the csv file
+data = {'Date' : [date], 'DOGE/USD' : [doge_price], 'Rank' : [doge_rank]}
+df_data = pd.DataFrame(data, columns = ['Date', 'DOGE/USD', 'Rank'])
+df_update = pd.read_csv('DogeValue.csv')
+
+# check for duplicated data
+df_yesterday = pd.Series(df_data.loc[0, 'Date'])
+if df_yesterday.isin(df_update['Date']).bool() == False:
+    # append new data to the table and save the updated file
+    df_update = df_update.append(df_data).sort_values(by = 'Date', ascending = False)
+    df_update.to_csv('DogeValue.csv', index = False)
+else:
+    print("There are duplicated data")
+
